26aaad2637969f7df5582856763d7e0bd54b8ed9
Nic McPhee <mcphee@morris.umn.edu>
2025-04-26 14:21:35 -0500

    Solve Part 1 of Quest 1

--- src/bin/quest_1_part_1.rs
+++ src/bin/quest_1_part_1.rs
@@ -1,9 +1,20 @@
 fn main() {
-    println!("Hello world");
+    let creatures = include_str!("../inputs/quest_1.txt");
+    let result = num_potions(creatures);
+    println!("We need {result} potions.");
 }
 
-fn num_potions(creatures: String) -> u32 {
-    todo!()
+fn num_potions(creatures: &str) -> u32 {
+    creatures.chars().map(potions_for_creature).sum()
+}
+
+fn potions_for_creature(creature: char) -> u32 {
+    match creature {
+        'A' => 0,
+        'B' => 1,
+        'C' => 3,
+        c => panic!("Should never see character '{c}'!"),
+    }
 }
 
 #[cfg(test)]
@@ -12,7 +23,7 @@ mod tests {
 
     #[test]
     fn example() {
-        let creatures = "ABBAC".to_string();
+        let creatures = "ABBAC";
         let result = num_potions(creatures);
         assert_eq!(result, 5);
     }
